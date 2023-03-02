b695ab0c91c4dceb394cc423bdfd761d70e7125c
Yousef Mustafa <124099534+nvv1@users.noreply.github.com>
2023-03-02 22:11:46 +0200

    ara

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,30 +31,38 @@ const generatePDF = async (name) => {
   pdfDoc.registerFontkit(fontkit);
 
   //get font
-  const fontBytes = await fetch("./Sanchez-Regular.ttf").then((res) =>
+  const fontBytes = await fetch("./Amiri-Regular.ttf").then((res) =>
     res.arrayBuffer()
   );
 
   // Embed our custom font in the document
-  const SanChezFont = await pdfDoc.embedFont(fontBytes);
+  const AmiriFont = await pdfDoc.embedFont(fontBytes);
 
   // Get the first page of the document
   const pages = pdfDoc.getPages();
   const firstPage = pages[0];
 
-  // Draw a string of text diagonally across the first page
-  firstPage.drawText(name, {
-    x: 200,
-    y: 330,
-    size: 40,
-    font: SanChezFont,
+  // Draw a string of Arabic text diagonally across the first page
+  const { width, height } = firstPage.getSize();
+  const fontSize = 40;
+  const text = name;
+  const x = width / 2 - AmiriFont.widthOfTextAtSize(text, fontSize) / 2;
+  const y = height / 2 - fontSize / 2;
+  firstPage.drawText(text, {
+    x,
+    y,
+    font: AmiriFont,
+    size: fontSize,
     color: rgb(0.2, 0.84, 0.67),
+    lineHeight: 1.5,
+    textAlign: "center",
+    direction: "rtl",
+    rotate: degrees(-45),
   });
 
   // Serialize the PDFDocument to bytes (a Uint8Array)
   const pdfBytes = await pdfDoc.save();
   console.log("Done creating");
-
 
   var file = new File(
     [pdfBytes],
@@ -63,7 +71,5 @@ const generatePDF = async (name) => {
       type: "application/pdf;charset=utf-8",
     }
   );
- saveAs(file);
+  saveAs(file);
 };
-
-
